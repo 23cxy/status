0c1917b6ca4e923550aa531b04f86eefeee85bb3
23cxy <23cxy@users.noreply.github.com>
2020-07-29 11:28:35 +0000

    deploy: 23cxy/com.23cxy.status@eb875ac4f7f968784fa7a392de8b5741f2f62e06

--- config.js
+++ config.js
@@ -13,12 +13,12 @@ window.Config = {
     'm785164209-189250b818f8cab885889b2d',  // 23cxy.com
     'm785568777-26848f64bfe168b703262d06',  // kms.23cxy.com
     'm785568808-78ac3914a6ba64c521302105',  // status.23cxy.com
-    'm785591353-f0036697c1f6f3d6b3bbd67f',  // 23cxy.art
-    'm785591350-3e1e53c408aada85d37e934d',  // 23cxy.club
+    // 'm785591353-f0036697c1f6f3d6b3bbd67f',  // 23cxy.art
+    // 'm785591350-3e1e53c408aada85d37e934d',  // 23cxy.club
     'm785591349-ca420c934301ac6f2bf0107b',  // 23cxy.me
     'm785591351-ef3225fd824bd86054ca84f8',  // 23cxy.shop
-    'm785591354-07798c602c5ddcc0665982a7',  // 23cxy.site
-    'm785591355-f950639a78117b4c1c73feae',  // 23cxy.tech
+    // 'm785591354-07798c602c5ddcc0665982a7',  // 23cxy.site
+    // 'm785591355-f950639a78117b4c1c73feae',  // 23cxy.tech
     'm785568768-090e9a2e3e647b214e7aae2a',  // github.com
   ],
 
