c6252639103c987b2fd072695a1955fc6b5021ae
23cxy <23cxy@users.noreply.github.com>
2020-10-28 06:59:45 +0000

    deploy: 23cxy/com.23cxy.status@dec2dccc7e4f91a3318eaa92431d27abe927d20a

--- config.js
+++ config.js
@@ -1,14 +1,7 @@
 // 配置
 window.Config = {
-
-  // 站点名
   SiteName: '爱上程序员',
-
-  // 站点链接
   SiteUrl: 'https://status.23cxy.com',
-
-  // UptimeRobot Api Keys
-  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
     'm785164209-189250b818f8cab885889b2d',  // 23cxy.com
     'm786234498-e64e417edf918e6d9bad49ed',  // oqu.me
@@ -18,16 +11,8 @@ window.Config = {
     'm785591351-ef3225fd824bd86054ca84f8',  // 23cxy.shop
     'm785568768-090e9a2e3e647b214e7aae2a',  // github.com
   ],
-
-  // 是否显示监测站点的链接
   ShowLink: true,
-
-  // 日志天数
-  // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
-  // 不过时间不要设置太长，容易卡，接口请求也容易失败
   CountDays: 60,
-
-  // 导航栏菜单
   Navi: [
     {
       text: '返回主站',
