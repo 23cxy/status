7460b6a9a011f238f62497ad76d72f365e747956
23cxy <23cxy@users.noreply.github.com>
2020-10-28 06:13:18 +0000

    deploy: 23cxy/com.23cxy.status@74425142b0962ad3dd7a38403b48b06ddc803210

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
     'm785164209-189250b818f8cab885889b2d',  // 23cxy.com
-    'm785568808-78ac3914a6ba64c521302105',  // status.23cxy.com
     'm786234498-e64e417edf918e6d9bad49ed',  // oqu.me
+    'm785568808-78ac3914a6ba64c521302105',  // status.23cxy.com
     'm785591349-ca420c934301ac6f2bf0107b',  // 23cxy.me
     'm785591351-ef3225fd824bd86054ca84f8',  // 23cxy.shop
     'm785568768-090e9a2e3e647b214e7aae2a',  // github.com
