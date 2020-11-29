89e66d414c8990e0a51e3781765b26c395686b1d
23cxy <23cxy@users.noreply.github.com>
2020-11-29 10:55:14 +0000

    deploy: 23cxy/com.23cxy.status@135fcad4660e8518d59cac953415dd077100e5a8

--- config.js
+++ config.js
@@ -5,7 +5,6 @@ window.Config = {
   ApiKeys: [
     'm785164209-189250b818f8cab885889b2d',  // 23cxy.com
     'm786234498-e64e417edf918e6d9bad49ed',  // oqu.me
-    'm785164211-f64c9378b26f2a2d163392ae',  // qingmi.icu
     'm785568808-78ac3914a6ba64c521302105',  // status.23cxy.com
     'm785591349-ca420c934301ac6f2bf0107b',  // 23cxy.me
     'm785591351-ef3225fd824bd86054ca84f8',  // 23cxy.shop
