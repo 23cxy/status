a4c74009c1421edc4cbf088941cc61e4ccf5bdf1
23cxy <23cxy@users.noreply.github.com>
2021-01-17 13:55:43 +0000

    deploy: 23cxy/com.23cxy.status@5efc0e939704babb46e50861a77763177b7dd39f

--- config.js
+++ config.js
@@ -4,10 +4,11 @@ window.Config = {
   SiteUrl: 'https://status.23cxy.com',
   ApiKeys: [
     'm785164209-189250b818f8cab885889b2d',  // 23cxy.com
-    'm786234498-e64e417edf918e6d9bad49ed',  // oqu.me
-    'm785568808-78ac3914a6ba64c521302105',  // status.23cxy.com
     'm785591349-ca420c934301ac6f2bf0107b',  // 23cxy.me
     'm785591351-ef3225fd824bd86054ca84f8',  // 23cxy.shop
+    'm785568808-78ac3914a6ba64c521302105',  // status.23cxy.com
+    'm786947388-2c9ce6b1af37a2a3d4d8b058',  // mikutap.23cxy.com
+    'm786234498-e64e417edf918e6d9bad49ed',  // oqu.me
     'm785568768-090e9a2e3e647b214e7aae2a',  // github.com
   ],
   ShowLink: true,
